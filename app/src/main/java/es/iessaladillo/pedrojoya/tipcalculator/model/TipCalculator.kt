78af72a173a29b91a9aa7ba0e2aaa84dcf9a9f3e
Ismael Reyes Caballero <ismaelreyescaballero@gmail.com>
2019-10-27 17:37:05 +0100

    TipCalculator v1.1
    - Changed calculatePerDinerRounded() method. Now it rounds a number up correctly.

--- app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/tipcalculator/model/TipCalculator.kt
@@ -1,12 +1,12 @@
 package es.iessaladillo.pedrojoya.tipcalculator.model
 
 import es.iessaladillo.pedrojoya.tipcalculator.R
-import kotlin.math.ceil
+import kotlin.math.roundToInt
 
 
 // TipCalculator class. Its constructor receives bill, percentage and diners
 
-class TipCalculator(var bill: Float, var percentage: Float, var diners: Int) {
+class TipCalculator(private var bill: Float, private var percentage: Float, private var diners: Int) {
 
     init {
         require(bill >= 0) { R.string.invalidBill }
@@ -27,23 +27,19 @@ class TipCalculator(var bill: Float, var percentage: Float, var diners: Int) {
     }
 
     fun calculatePerDinerRounded(): Float {
-        val perDiner = calculateTotal() / diners
-        val perDinerRounded = ceil((perDiner).toDouble()).toFloat()
-        return if (isDecimalZero(perDiner) && perDiner != perDinerRounded) {
-            // If perDiner ends with .00 and is not equal to perDinerRounded, returns perDinerRounded - 1.
-            // This is because the app doesn't show the exact float value, but it can be slightly different.
-            // Example: 4.00 and 4.004. It should not round up to 5.00 because the user cannot see that
-            // the value is really 4.004, and instead sees 4.00.
-            perDinerRounded - 1
+        val perDiner = calculatePerDiner()
+        val result = perDiner.roundToInt().toFloat()
+        // We check if perDiner shows two zeros as decimals to prevent rounding up when it shouldn't.
+        return if (result < perDiner && !areDecimalsZero(perDiner)) {
+            result + 1
         } else {
-            perDinerRounded
+            result
         }
     }
 
     // This function checks if a float value has .00 as decimals.
-    private fun isDecimalZero(num: Float): Boolean {
-        // Number is turned into a string with six decimals to prevent rounding, and the comma is replaced with a dot.
-        val numString = String.format("%.6f", num).replace(",", ".")
+    private fun areDecimalsZero(num: Float): Boolean {
+        val numString = String.format("%.2f", num).replace(",", ".")
         val dotPosition = numString.indexOf(".", 0, false)
         val subString = numString.substring(dotPosition + 1, dotPosition + 3)
         return subString == "00"
